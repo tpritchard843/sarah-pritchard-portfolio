cc0e395a528febe15934e8bfe6dec8e09fef4f90
unexpectedhaki <thomas.pritchard20@gmail.com>
2023-02-20 20:41:43 -0500

    Added data file This file houses an array full of portfolio video objects. Each object contains an image, a URL, and a caption/title.
    My intention is to render the portfolio section dynamically, rather than hardcoding each object in HTML. This will allow me to add videos to the portfolio as they are uploaded, and not have to play tetris to get the order right.

---
+++ data.js
@@ -0,0 +1,67 @@
+export const portfolioVideosArr = [
+    {
+        image:"images/wvlt6-4.webp.webp",
+        url:"https://youtu.be/0HjAU_QFpwQ",
+        caption:"WVLT News at 6 - 02/15/2023"
+    },
+    {
+        image:"https://youtu.be/aMVXE1vSRWY",
+        url:"images/wvlt6-3.webp",
+        caption:"WVLT News at 6 - 02/14/2023"
+    },
+    {
+        image:"images/wvlt6-feb6.webp",
+        url:"https://youtu.be/1Shk6yRbUlA",
+        caption:"WVLT News at 6 - 02/06/2023"
+    },
+    {
+        image:"images/wvlt6-2.webp",
+        url:"https://youtu.be/SJI_R42PlCE",
+        caption:"WVLT News at 6 - 01/12/2023"
+    },
+    {
+        image:"images/wvlt6-1.webp",
+        url:"https://youtu.be/rMeCvWm_Wdc",
+        caption:"WVLT News at 6 - 01/11/2023"
+    },
+    {
+        image:"images/a-block-example2.webp",
+        url:"https://youtu.be/_FtfsaTQPak",
+        caption:"6 p.m. A-Block - 11/30/2022"
+    },
+    {
+        image:"images/a-block-example1.webp",
+        url:"https://youtu.be/YHPloU3JUDk",
+        caption:"6 p.m. A-Block - 04/08/2022"
+    },
+    {
+        image:"images/flood-cleanup.webp",
+        url:"https://youtu.be/q9WAK6oH9uA",
+        caption:"Produced Live Shot - 08/24/2021"
+    },
+    {
+        image:"images/medal-of-honor1.webp",
+        url:"https://youtu.be/9J6ST87-Ox4",
+        caption:"Medal of Honor (Pt. 1) - 08/31/2022"
+    },
+    {
+        image:"images/team-coverage.webp",
+        url:"https://youtu.be/0uUpaPv_2JE",
+        caption:"Team Coverage - 04/01/2022"
+    },
+    {
+        image:"images/breaking-news.webp",
+        url:"https://youtu.be/7-dBKGMBJEg",
+        caption:"Breaking News - 06/08/2021"
+    },
+    {
+        image:"images/big-orange-tipoff.webp",
+        url:"https://youtu.be/gpEtK6WIIFM",
+        caption:"Big Orange Tipoff - 01/28/2023"
+    },
+    {
+        image:"images/medal-of-honor2.webp",
+        url:"https://youtu.be/YBbB7Y2-fwk",
+        caption:"Medal of Honor (Pt. 2) - 09/13/2022"
+    }
+]
